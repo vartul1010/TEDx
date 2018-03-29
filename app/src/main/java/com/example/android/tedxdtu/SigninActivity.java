b610560c7408b5b8e7b61e29bce214a5c992819c
Vartul <vartul.bhardwaj1998@gmail.com>
2018-03-29 14:07:34 +0530

    hmm

---
+++ app/src/main/java/com/example/android/tedxdtu/SigninActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.tedxdtu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vartul1010 on 29/3/18.
+ */
+
+public class SigninActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+
+        
+    }
+}
